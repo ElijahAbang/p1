a33309f3a1f5a7f26b009f7018fb6301a65e66f9
ElijahAbang <70170276+ElijahAbang@users.noreply.github.com>
2020-09-22 14:52:06 -0500

    Add project files.

---
+++ p1/Main.cpp
@@ -0,0 +1,31 @@
+/*
+Elijah Abang
+C++
+sep 22
+p1
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "what is your name?";
+	cin >> name;
+	cout << endl << "Name:" << name << endl;
+	
+	string age;
+	cout << "what is your age?";
+	cin >> age;
+	cout << endl << "Age:" <<age<< endl;
+	
+	string race;
+	cout << "what is your race?";
+	cin >> race;
+	cout << endl << "Race:" <<race<< endl;
+
+	return 0;
+}
